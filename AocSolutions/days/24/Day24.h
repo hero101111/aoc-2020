76150301fb8209b85afff1fd06d892e775163849
Gabriel Diaconita <diaconitagabriel@gmail.com>
2020-12-06 08:12:14 +0200

    Regenerate dummy solutions using UTF8 encoding

---
+++ AocSolutions/days/24/Day24.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "SolutionDay.h"
+#include "util/util.h"
+
+class Day24 : public ISolutionDay
+{
+private:
+
+  //using DataType = DynamicMap<char>;
+  using DataType = vector<string>;
+  DataType mData;
+
+public:
+
+  Day24() { }
+
+  ~Day24() override { }
+
+  string GetDay() override
+  {
+    return "24";
+  }
+
+  void ReadData()
+  {
+    mData = DataType();
+
+    //mData.fromfile(GetInputPath());
+    mData = rff(GetInputPath());
+  }
+
+  LL DoWork()
+  {
+    LL ret = 0;
+
+    // const auto matches = RegexMatch(passData, R"()");
+    return ret;
+  }
+
+  string Part1() override
+  {
+    ReadData();
+
+    return std::to_string(DoWork());
+  }
+
+  string Part2() override
+  {
+    ReadData();
+
+    return std::to_string(DoWork());
+  }
+
+  bool Test() override
+  {
+    {
+      mCurrentInput = "test";
+      //assert(Part1() == "");
+      //assert(Part2() == "");
+    }
+    {
+      mCurrentInput = "input";
+      //assert(Part1() == "");
+      //assert(Part2() == "");
+    }
+    return ISolutionDay::Test();
+  }
+};
