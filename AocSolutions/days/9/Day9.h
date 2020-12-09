cb9c3f042408b856a94e402ccb789e23232aca80
Gabriel Diaconita <diaconitagabriel@gmail.com>
2020-12-09 07:16:50 +0200

    Add solution for Day9

--- AocSolutions/days/9/Day9.h
+++ AocSolutions/days/9/Day9.h
@@ -7,7 +7,6 @@ class Day9 : public ISolutionDay
 {
 private:
 
-  //using DataType = DynamicMap<char>;
   using DataType = vector<string>;
   DataType mData;
 
@@ -25,44 +24,78 @@ public:
   void ReadData()
   {
     mData = DataType();
-
-    //mData.fromfile(GetInputPath());
     mData = rff(GetInputPath());
   }
 
-  LL DoWork()
+  LL DoWork(bool partTwo)
   {
-    LL ret = 0;
+    vector<LL> nums;
+    for (auto d : mData)
+      nums.push_back(stoll(d));
 
-    // const auto matches = RegexMatch(passData, R"()");
-    return ret;
+    if (!partTwo)
+    {
+      int offset = 25;
+       for (int i = offset; i < nums.size(); ++i)
+       {
+         bool isValid = false;
+         for (int j = i - offset; j < i - 1; j++)
+           for (int k = j + 1; k < i; k++)
+           {
+             if (nums[j] + nums[k] == nums[i])
+               isValid = true;
+           }
+         if (!isValid)
+           return nums[i];
+       }
+    }
+    else
+    {
+      LL toFind = DoWork(false);
+
+      for (int i = 0; i < mData.size() - 1; i++)
+      {
+        for (int j = i + 1; j < mData.size(); j++)
+        {
+          LL sum = 0;
+          for (int k = i; k <= j; ++k)
+            sum += nums[k];
+          if (sum == toFind)
+          {
+            vector<LL> outs;
+            for (int k = i; k <= j; ++k)
+              outs.push_back(nums[k]);
+            sort(begin(outs), end(outs));
+
+            return *outs.begin() + *(outs.end() - 1);
+          }
+        }
+      }
+    }
+
+    return 0;
   }
 
   string Part1() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return std::to_string(DoWork(false));
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return std::to_string(DoWork(true));
   }
 
   bool Test() override
   {
     {
-      mCurrentInput = "test";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
-    }
-    {
       mCurrentInput = "input";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
+      assert(Part1() == "90433990");
+      assert(Part2() == "11691646");
     }
     return ISolutionDay::Test();
   }
