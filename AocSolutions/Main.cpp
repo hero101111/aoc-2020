15cb1131c6c40043aaab1cf175a01e453f81a1d1
Gabriel Diaconița <diaconitagabriel@gmail.com>
2020-12-13 00:32:31 +0200

    Add code for running all tests automatically

--- AocSolutions/Main.cpp
+++ AocSolutions/Main.cpp
@@ -40,8 +40,17 @@ int main(int argc, char** argv)
   REG_DAY(14);  REG_DAY(15);  REG_DAY(16);  REG_DAY(17); REG_DAY(18); REG_DAY(19);
   REG_DAY(20);  REG_DAY(21);  REG_DAY(22);  REG_DAY(23); REG_DAY(24); REG_DAY(25);
  
-  int DAY  = 12;
-  int PART = 2;
+  bool runAllTests = false;
+  if (runAllTests)
+  {
+    for (auto & runner : solutions)
+    {
+      cout << "Running tests for day " << runner.first << endl;
+      runner.second->Test();
+    }
+  }
+  int DAY  = 1;
+  int PART = 1;
 
   ///////////////////////////////
 
