274ffe8156f23a982ede5388412ef30ddb611351
Gabriel Diaconița <diaconitagabriel@gmail.com>
2020-12-14 08:07:39 +0200

    Add solution for Day 14

--- AocSolutions/days/14/Day14.h
+++ AocSolutions/days/14/Day14.h
@@ -9,6 +9,8 @@ private:
   //using DataType = DynamicMap<char>;
   using DataType = vector<string>;
   DataType mData;
+  
+  unordered_map<LL, LL> memory;
 
 public:
 
@@ -20,48 +22,149 @@ public:
   {
     return "14";
   }
+  
+  LL applyMask(LL value, string mask)
+  {
+    LL pw2 = 1;
+    for (LL i = mask.size() - 1; i >= 0; --i)
+    {
+      char crtBit = mask[i];
+      if (crtBit == '1')
+      {
+        value |= pw2;
+      }
+      if (crtBit == '0')
+      {
+        if ( (value & pw2) == pw2)
+        {
+          value -= pw2;
+        }
+      }
+      pw2 *= 2;
+    }
+    return value;
+  }
+  
+  void generateMemAddresses(string mask, vector<LL> & aOut, LL offset = 0)
+  {
+    vector<LL> ret;
+    
+    bool hasSwitches = false;
+    for (LL i = offset; i < mask.size(); ++i)
+    {
+      if (mask[i] == 'X')
+      {
+        hasSwitches = true;
+        
+        string mask1 = mask;
+        mask1[i] = '0';
+        generateMemAddresses(mask1, aOut, i);
+   
+        string mask2 = mask;
+        mask2[i] = '1';
+        generateMemAddresses(mask2, aOut, i);
+      
+        return;
+      }
+    }
+    if (!hasSwitches)
+    {
+      auto toAdd = stoll(mask, nullptr, 2);
+      aOut.push_back(toAdd);
+    }
+  }
+  
+  vector<LL> applyMemMask(LL memAddr, string mask)
+  {
+    LL pw2 = 1;
+    for (LL i = mask.size() - 1; i >= 0; --i)
+    {
+      char crtBit = mask[i];
+      if (crtBit == '0')
+      {
+        bool hasBit = ((memAddr & pw2) == pw2);
+        mask[i] = hasBit ? '1' : '0';
+      }
+      if (crtBit == '1')
+      {
+        mask[i] = '1';
+      }
+      
+      pw2 *= 2;
+    }
+    vector<LL> ret;
+    generateMemAddresses(mask, ret);
+    return ret;
+  }
 
   void ReadData()
   {
     mData = DataType();
 
-    //mData.fromfile(GetInputPath());
     mData = rff(GetInputPath());
   }
 
-  LL DoWork()
+  LL DoWork(bool partTwo)
   {
-    LL ret = 0;
+    memory.clear();
+    
+    string crtMask = "";
+    
+    for (auto d : mData)
+    {
+      const auto mask = RegexMatch(d, R"(mask = (.+))");
+      if (!mask.empty())
+      {
+        crtMask = mask[0];
+      }
+      
+      const auto mem = RegexMatch(d, R"(mem\[(\d+)\] = (.+))");
+      if (!mem.empty())
+      {
+        LL crtVal = stoll(mem[1]);
+        LL loc = stoll(mem[0]);
+      
+        if (!partTwo)
+        {
+          crtVal = applyMask(crtVal, crtMask);
+          memory[loc] = crtVal;
+        }
+        else
+        {
+          vector<LL> locations = applyMemMask(loc, crtMask);
+          for (auto locCell : locations)
+            memory[locCell] = crtVal;
+        }
+      }
+    }
+    
+    LL sum = 0;
+    for (auto [m, v] : memory)
+      sum += v;
 
-    // const auto matches = RegexMatch(passData, R"()");
-    return ret;
+    return sum;
   }
 
   string Part1() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return std::to_string(DoWork(false));
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return std::to_string(DoWork(true));
   }
 
   bool Test() override
   {
     {
-      mCurrentInput = "test";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
-    }
-    {
       mCurrentInput = "input";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
+      assert(Part1() == "8471403462063");
+      assert(Part2() == "2667858637669");
     }
     return ISolutionDay::Test();
   }
