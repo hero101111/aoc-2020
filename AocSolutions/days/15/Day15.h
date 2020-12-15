ab6dff5da7ef44e24dfba52992a38d59affdf07f
Gabriel Diaconița <diaconitagabriel@gmail.com>
2020-12-15 07:50:13 +0200

    Add solution for Day 15

--- AocSolutions/days/15/Day15.h
+++ AocSolutions/days/15/Day15.h
@@ -7,7 +7,7 @@ class Day15 : public ISolutionDay
 private:
 
   //using DataType = DynamicMap<char>;
-  using DataType = vector<string>;
+  using DataType = vector<LL>;
   DataType mData;
 
 public:
@@ -26,42 +26,76 @@ public:
     mData = DataType();
 
     //mData.fromfile(GetInputPath());
-    mData = rff(GetInputPath());
+    vector<string> fileData = rff(GetInputPath());
+    auto tokens = tok(fileData[0], ',');
+    for (auto t : tokens)
+      mData.push_back(stoll(t));
   }
 
-  LL DoWork()
+  LL DoWork(LL search)
   {
-    LL ret = 0;
-
-    // const auto matches = RegexMatch(passData, R"()");
-    return ret;
+    LL turn = 0;
+    LL turnNum = -1;
+    unordered_map<LL, vector<LL>> turnMap;
+    
+    LL counter = 0;
+    for (LL i : rangeint(1, mData.size()))
+    {
+      turn++;
+      turnNum = mData[i - 1];
+      turnMap[turnNum].push_back(turn);
+    }
+    
+    while (true)
+    {
+      if (turn == search)
+      {
+        return turnNum;
+      }
+      
+      turn++;
+      if (turnMap[turnNum].size() == 1)
+      {
+        turnNum = 0;
+      }
+      else
+      {
+        auto & vec = turnMap[turnNum];
+        turnNum = vec[vec.size() - 1] - vec[vec.size() - 2];
+      }
+      
+      turnMap[turnNum].push_back(turn);
+      
+      if (turnMap[turnNum].size() > 2)
+      {
+        turnMap[turnNum].erase(begin(turnMap[turnNum]));
+      }
+      
+      counter++;
+    }
+    return 0;
   }
 
   string Part1() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return std::to_string(DoWork(2020));
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return std::to_string(DoWork(30000000));
   }
 
   bool Test() override
   {
     {
-      mCurrentInput = "test";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
-    }
-    {
       mCurrentInput = "input";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
+      assert(Part1() == "273");
+      assert(Part2() == "47205");
     }
     return ISolutionDay::Test();
   }
