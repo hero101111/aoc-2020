4d96fb71aa8cb8390184bcee79286cddeb89019b
Gabriel Diaconita <diaconitagabriel@gmail.com>
2020-12-23 22:48:25 +0200

    Streamline solution for Day 23

--- AocSolutions/days/23/Day23.h
+++ AocSolutions/days/23/Day23.h
@@ -6,7 +6,6 @@ class Day23 : public ISolutionDay
 {
 private:
 
-  //using DataType = DynamicMap<char>;
   using DataType = vector<string>;
   DataType mData;
 
@@ -25,56 +24,24 @@ public:
 
   void ReadData()
   {
-    mData = DataType();
-
-    //cups = stoll(tok("418976235"s));
-
-
-    
+    mData = DataType();    
     cups = { 4,1,8,9,7,6,2,3,5 };
-     //cups = {3, 8, 9, 1, 2, 5, 4, 6, 7  };
-     // 4,1,8,9,7,6,2,3,5
-     //mData.fromfile(GetInputPath());
     mData = rff(GetInputPath());
   }
 
-  LL DoWork()
+  string DoWork(bool partTwo)
   {
-    LL ret = 231;
-
-    auto getIndex = [&](LL val)
+    if (partTwo)
     {
-
-      /*auto it = find(begin(cups), end(cups), val);
-      assert(it != end(cups));
-      return it - begin(cups);*/
-    };
-
-    LL startInsert = 10;
-    while (cups.size() < 1'000'000)
-    {
-      cups.push_back(startInsert);
-      startInsert++;
+      LL startInsert = 10;
+      while (cups.size() < 1'000'000)
+      {
+        cups.push_back(startInsert);
+        startInsert++;
+      }
     }
     
     unordered_map<LL, list<LL>::iterator> itCache;
-
-    auto getMin = [&]() -> LL
-    {
-      for (LL s = 1; s <= 1'000'000; ++s)
-        if (itCache.find(s) != end(itCache))
-          return s;
-      return 1;
-    };
-
-    auto getMax = [&]() -> LL
-    {
-      for (LL s = 1'000'000; s >= 1; --s)
-        if (itCache.find(s) != end(itCache))
-          return s;
-      
-      return 1;
-    };
 
     vector<LL> cache;
     {
@@ -86,66 +53,42 @@ public:
       }
     }
 
-    //verbose << join(cups, " ");
-
-    LL picked1, picked2, picked3;
     auto crt = cups.begin();
-    for (LL step = 0; step < 10'000'000; step++)
+    for (LL step = 0; step < (partTwo ? 10'000'000 : 100); step++)
     {
-      if (step % 1000 == 0)
-        cout << step << endl;
       auto pickIt = crt;
      
       advance(pickIt, 1);
-      if (pickIt == end(cups))
-        pickIt = cups.begin();
-      picked1 = *pickIt;
-      itCache.erase(picked1);
-      auto toRemove1 = pickIt;
-      advance(pickIt, 1);
-      cups.erase(toRemove1);
-  
-      if (pickIt == end(cups))
-        pickIt = cups.begin();
-      picked2 = *pickIt;
-      itCache.erase(picked2);
-      auto toRemove2 = pickIt;
-      advance(pickIt, 1);
-      cups.erase(toRemove2);
 
+      vector<LL> picked;
+      for (int i : rangeint(1, 3))
+      {
+        if (pickIt == end(cups))
+          pickIt = cups.begin();
+        picked.push_back(*pickIt);
+        itCache.erase(*pickIt);
+        auto toRemove1 = pickIt;
+        advance(pickIt, 1);
+        cups.erase(toRemove1);
+      }
 
-      if (pickIt == end(cups))
-        pickIt = cups.begin();
-      picked3 = *pickIt;
-      itCache.erase(picked3);
-      auto toRemove3 = pickIt;
-      cups.erase(toRemove3);
-
-      LL lowest = getMin();
-      LL highest = getMax();
-
-      LL destVal = *crt - 1;
-      while (picked1 == destVal || picked2 == destVal || picked3 == destVal)
+      LL destVal = *crt;
+      do
       {
         destVal -= 1;
-      }
+        if (destVal < 1)
+          destVal = partTwo ? 1'000'000 : 9;
+      } while (contains(picked, destVal));
 
-      if (destVal < lowest)
-      {
-        destVal = highest;
-      }
 
       auto destIt = itCache[destVal];
       advance(destIt, 1);
 
-      destIt = cups.insert(destIt, picked3);
-      itCache[picked3] = destIt;
-
-      destIt = cups.insert(destIt, picked2);
-      itCache[picked2] = destIt;
-
-      destIt = cups.insert(destIt, picked1);
-      itCache[picked1] = destIt;
+      for (auto pickedIt = picked.rbegin(); pickedIt != picked.rend(); ++pickedIt)
+      {
+        destIt = cups.insert(destIt, *pickedIt);
+        itCache[*pickedIt] = destIt;
+      }
 
       if (*crt == cups.back())
         crt = cups.begin();
@@ -153,57 +96,56 @@ public:
         advance(crt, 1);
     }
 
-    auto it = cups.begin();
-    while (*it != 1)
+    if (partTwo)
+    {
+      auto it = cups.begin();
+      while (*it != 1)
+        advance(it, 1);
+
       advance(it, 1);
+      if (it == end(cups))
+        it = begin(cups);
+      LL val1 = *it;
 
-    advance(it, 1);
-    if (it == end(cups))
-      it = begin(cups);
-    LL val1 = *it;
-
-    advance(it, 1);
-    if (it == end(cups))
-      it = begin(cups);
-    LL val2 = *it;
-
-    cout << "values are " << val1 << " " << val2 << endl;
-    ret = val1 * val2;
-
-    /*string val;
-    for (auto l : cups)
-      val += to_string(l);
-    cout << val;*/
-
-    // const auto matches = RegexMatch(passData, R"()");
-    return ret;
+      advance(it, 1);
+      if (it == end(cups))
+        it = begin(cups);
+      LL val2 = *it;
+ 
+      string ret = to_string(val1 * val2);
+      return ret;
+    }
+    else
+    {
+      string ret = join(cups, "");
+      size_t posOf1 = ret.find('1');
+      assert(posOf1 != string::npos);
+      ret = ret.substr(posOf1 + 1) + ret.substr(0, posOf1);
+      return ret;
+    }
   }
 
   string Part1() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return DoWork(false);
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return DoWork(true);
   }
 
   bool Test() override
   {
     {
-      mCurrentInput = "test";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
-    }
-    {
       mCurrentInput = "input";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
+      auto p1 = Part1();
+      assert(Part1() == "96342875");
+      assert(Part2() == "563362809504");
     }
     return ISolutionDay::Test();
   }
