611e5fa83c2e90d228d56ef415e48b3375213ee7
Gabriel Diaconita <diaconitagabriel@gmail.com>
2020-12-25 07:37:55 +0200

    Add solution for Day 25

--- AocSolutions/days/25/Day25.h
+++ AocSolutions/days/25/Day25.h
@@ -6,11 +6,13 @@ class Day25 : public ISolutionDay
 {
 private:
 
-  //using DataType = DynamicMap<char>;
   using DataType = vector<string>;
   DataType mData;
 
 public:
+
+  LL cardPubKey, doorPubKey;
+  int cardLoop = 1, doorLoop = 1;
 
   Day25() { }
 
@@ -25,43 +27,77 @@ public:
   {
     mData = DataType();
 
-    //mData.fromfile(GetInputPath());
     mData = rff(GetInputPath());
+
+    cardPubKey = stoll(mData[0]);
+    doorPubKey = stoll(mData[1]);
   }
 
-  LL DoWork()
+  int FindLoopNumber(LL subject, LL searched)
   {
-    LL ret = 0;
+    LL value = 1;
+    int count = 1;
+    while (true)
+    {
+      value *= subject;
+      value = value % 20201227;
 
-    // const auto matches = RegexMatch(passData, R"()");
-    return ret;
+      if (value == searched)
+        return count;
+
+      count++;
+    }
+    return count;
+  }
+
+
+  LL Encode(LL subject, int loopLimit)
+  {
+    LL value = 1;
+    int count = 1;
+    while (true)
+    {
+      value *= subject;
+      value = value % 20201227;
+
+      count++;
+      if (count > loopLimit)
+        return value;
+
+    }
+    return value;
+  }
+
+  string DoWork(bool partTwo)
+  {
+    if (partTwo)
+      return "MERRY CHRISTMAS";
+
+    cardLoop = FindLoopNumber(7, cardPubKey);
+    LL encKey = Encode(doorPubKey, cardLoop);
+    return to_string(encKey);
   }
 
   string Part1() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return DoWork(false);
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork());
+    return DoWork(true);
   }
 
   bool Test() override
   {
     {
-      mCurrentInput = "test";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
-    }
-    {
       mCurrentInput = "input";
-      //assert(Part1() == "");
-      //assert(Part2() == "");
+      assert(Part1() == "7269858");
+      assert(Part2() == "MERRY CHRISTMAS");
     }
     return ISolutionDay::Test();
   }
